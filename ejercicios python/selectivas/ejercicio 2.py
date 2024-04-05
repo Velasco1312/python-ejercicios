5a82cf9396efe3f97c3c2024ce8b1a80e5f2c3f2
Velasco1312 <151095253+Velasco1312@users.noreply.github.com>
2024-04-05 16:59:24 -0500

    Add files via upload

---
+++ ejercicios python/selectivas/ejercicio 2.py
@@ -0,0 +1,9 @@
+#problema de dar el valor de una compra y darle un descuento
+valor=int(input("ingresar valor de compra"))
+descuento=0.5
+totalcompra=0
+totalCompra=valor-(valor*descuento)
+
+print(f" el valor de la compra es: ,{valor}")
+print(f"el descuente fue de: {valor}*{descuento}")
+print(" la cantidad a pagar es ",totalCompra)
